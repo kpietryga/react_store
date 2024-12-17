551e05716cebe2a0ce24be43933a36500d8a2d40
kpietryga <krzysztof.pietryga@gmail.com>
2024-12-17 22:30:56 +0100

    add PromotedProducts.tsx

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,5 +1,15 @@
 import React from "react";
+import PromotedProducts from "./PromotedProducts";
 
-const Products: React.FC = () => <h1 className="text-4xl font-bold text-blue-500 m-10">Products</h1>;
 
-    export default Products;
+const Products: React.FC = () => {
+
+    return (
+        <div>
+            <h1 className="text-4xl font-bold text-blue-500 m-10">Products</h1>
+            <PromotedProducts/>
+        </div>
+    )
+}
+
+export default Products;
---
+++ src/components/PromotedProducts.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Products: React.FC = () => <h1 className="text-4xl font-bold text-blue-500 m-10">PromotedProducts</h1>;
+
+    export default Products;
