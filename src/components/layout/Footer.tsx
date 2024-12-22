d09bbf16934a884c18c79efefd140c136e9398ca
kpietryga <krzysztof.pietryga@gmail.com>
2024-12-22 23:06:03 +0100

    add top footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,8 +1,18 @@
 const Footer = () => {
     return (
-        <footer className="footer w-full bg-[#272829] text-white p-6">
-            <div className="max-w-7xl text-center m-auto">
-                <p>© 2024 Moja Firma. Wszystkie prawa zastrzeżone.</p>
+        <footer className="footer ">
+            <div className="max-w-7xl text-center m-auto flex justify-between p-9">
+                <div>a</div>
+                <div>b</div>
+                <div>c</div>
+                <div>d</div>
+            </div>
+
+            <div className=" bg-[#272829] text-white p-9">
+                <div className="max-w-7xl text-center m-auto">
+                    <p>© 2024 Moja Firma. Wszystkie prawa zastrzeżone.</p>
+                </div>
+
             </div>
 
         </footer>
