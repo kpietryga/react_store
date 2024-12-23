69a5212db784d681738308e7f8e30b9e2de775ae
kpietryga <krzysztof.pietryga@gmail.com>
2024-12-23 23:48:56 +0100

    add text in footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,10 +2,24 @@ const Footer = () => {
     return (
         <footer className="footer ">
             <div className="max-w-7xl text-center m-auto flex justify-between p-9">
-                <div>a</div>
-                <div>b</div>
-                <div>c</div>
-                <div>d</div>
+                <div>Oferujemy bukiety, upominki i kosze na każdą możliwą okazję. Dzięki wieloletniemu doświadczeniu we florystyce spełnimy wszystkie Twoje życzenia.</div>
+                <div>Godziny otwarcia
+                    Poniedziałek-Piątek:	7:00 - 19:00
+                    Sobota:	7:00 - 19:00
+                    Niedziela:	9:00 - 17:00
+                </div>
+                <div>
+                    ul. Tarnogórska 14
+                    44-100 Gliwice
+
+                    (+48) 32 230 48 58
+                    (+48) 781 360 260
+                    (+48) 12 312 43 10
+                    (+48) 61 415 18 12
+
+                    info@roza-kwiaty.pl</div>
+                <div>Obsługiwane miasta
+                    Gliwice | Bytom | Chorzów | Katowice | Knurów | Mikołów | Orzesze | Pyskowice | Ruda Śląska | Rybnik | Tychy | Zabrze | pozostałe miasta »</div>
             </div>
 
             <div className=" bg-[#272829] text-white p-9">
