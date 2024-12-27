5ff7fe724ec0ce8c85af97c876f4c5c8304e08ca
kpietryga <krzysztof.pietryga@gmail.com>
2024-12-27 23:49:53 +0100

    Edit footer style

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,19 +1,21 @@
 import {ADDRESS, CITIES, EMAIL, PHONE_NUMBERS} from "../common/ContactData.tsx";
 
+
 const Footer = () => {
+const divClass = "flex-1 p-6 bg-gray-100 rounded-xl m-6"
     return (
         <footer className="footer ">
             <div className="max-w-7xl text-center m-auto flex justify-between p-9">
-                <div className="flex-1 p-6 bg-rose-50 rounded-xl m-6">Oferujemy bukiety, upominki i kosze na każdą możliwą okazję. Dzięki
+                <div className="flex-1 p-6 bg-gray-100 rounded-xl m-6">Oferujemy bukiety, upominki i kosze na każdą możliwą okazję. Dzięki
                     wieloletniemu doświadczeniu we florystyce spełnimy wszystkie Twoje życzenia.
                 </div>
-                <ul className="flex-1 p-6 bg-rose-50 rounded-xl m-6">
+                <ul className={divClass}>
                     Godziny otwarcia
                     <li>Poniedziałek-Piątek: 7:00 - 19:00</li>
                     <li>Sobota: 7:00 - 19:00</li>
                     <li>Niedziela: 9:00 - 17:00</li>
                 </ul>
-                <div className=" flex-1 p-6 bg-rose-50 rounded-xl m-6">
+                <div className={divClass}>
                     <p>{ADDRESS.street} {ADDRESS.streetNumber}</p>
                     <p>{ADDRESS.postNumber} {ADDRESS.city}</p>
 
@@ -25,8 +27,9 @@ const Footer = () => {
 
                     <p>{EMAIL}</p>
                 </div>
-                <div className="flex-1 p-6 bg-rose-50 rounded-xl m-6">Obsługiwane miasta:
-                    {CITIES.map((city, i) => (<div key={"nr" + i}>{city}</div>))}
+                <div className={divClass}>
+                    <p className="font-bold">Obsługiwane miasta:</p>
+                    {CITIES.map((city, i) => (<div key={"nr" + i} className="inline-block ml-1"> {city} </div>))}
                 </div>
             </div>
 
