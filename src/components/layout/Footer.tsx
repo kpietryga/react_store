b9f194d57cc55ba7b913deedd7733e243b98c1ed
kpietryga <krzysztof.pietryga@gmail.com>
2024-12-28 22:22:59 +0100

    Edit footer style

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,26 +1,34 @@
 import {ADDRESS, CITIES, EMAIL, PHONE_NUMBERS} from "../common/ContactData.tsx";
-
+import rozaLogo from "../../assets/roza-logo.png";
+const divClass = "flex-1 py-6 text-white text-[14px] text-left"
 
 const Footer = () => {
-const divClass = "flex-1 p-6 bg-gray-100 rounded-xl m-6"
+
     return (
-        <footer className="footer ">
-            <div className="max-w-7xl text-center m-auto flex justify-between p-9">
-                <div className="flex-1 p-6 bg-gray-100 rounded-xl m-6">Oferujemy bukiety, upominki i kosze na każdą możliwą okazję. Dzięki
+        <footer className="footer px-9 bg-[#272829]  ">
+            <div className="max-w-7xl text-center m-auto flex justify-between gap-20">
+                <div className={divClass + " text-left"}>
+                    <img src={rozaLogo} alt="Logo" className="h-8 w-auto mb-2"/>
+                    Oferujemy bukiety, upominki i kosze na każdą możliwą okazję. Dzięki
                     wieloletniemu doświadczeniu we florystyce spełnimy wszystkie Twoje życzenia.
                 </div>
-                <ul className={divClass}>
-                    Godziny otwarcia
-                    <li>Poniedziałek-Piątek: 7:00 - 19:00</li>
-                    <li>Sobota: 7:00 - 19:00</li>
-                    <li>Niedziela: 9:00 - 17:00</li>
-                </ul>
+
                 <div className={divClass}>
-                    <p>{ADDRESS.street} {ADDRESS.streetNumber}</p>
+                    <p className="font-bold mb-2">Godziny otwarcia</p>
+                    <ul>
+                        <li>Poniedziałek-Piątek: 7:00 - 19:00</li>
+                        <li>Sobota: 7:00 - 19:00</li>
+                        <li>Niedziela: 9:00 - 17:00</li>
+                    </ul>
+                    <p className="mt-2">{ADDRESS.street} {ADDRESS.streetNumber}</p>
                     <p>{ADDRESS.postNumber} {ADDRESS.city}</p>
+                </div>
+
+                <div className={divClass}>
+
 
                     <ul>
-                        {PHONE_NUMBERS.map((nr, i) => (
+                    {PHONE_NUMBERS.map((nr, i) => (
                             <li key={"nr" + i}>{nr}</li>
                         ))}
                     </ul>
@@ -28,14 +36,14 @@ const divClass = "flex-1 p-6 bg-gray-100 rounded-xl m-6"
                     <p>{EMAIL}</p>
                 </div>
                 <div className={divClass}>
-                    <p className="font-bold">Obsługiwane miasta:</p>
+                    <p className="font-bold mb-2">Obsługiwane miasta:</p>
                     {CITIES.map((city, i) => (<div key={"nr" + i} className="inline-block ml-1"> {city} </div>))}
                 </div>
             </div>
 
-            <div className=" bg-[#272829] text-white p-9">
-                <div className="max-w-7xl text-center m-auto">
-                    <p>© 2024 Moja Firma. Wszystkie prawa zastrzeżone.</p>
+            <div className="text-white p-9">
+                <div className="max-w-7xl  text-[14px] text-center m-auto border-t-[1px] border-t-[#3B3C3D] pt-10">
+                    <p>© 2024 Kwiaciarnia Róża. Wszystkie prawa zastrzeżone.</p>
                 </div>
 
             </div>
