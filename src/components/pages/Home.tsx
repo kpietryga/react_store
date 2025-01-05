ffb9301518704ec827969d148061e4433d72796a
kpietryga <krzysztof.pietryga@gmail.com>
2025-01-05 23:19:03 +0100

    edit product category

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,66 +1,77 @@
 import React from "react";
 //import {HomeIcon} from '@heroicons/react/24/outline';
 //import hero from '../../assets/hero.jpg';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
+// import {Swiper, SwiperSlide} from 'swiper/react';
+// import 'swiper/css';
 
 
 const Home: React.FC = () => {
 
-    const texts = [
-        "Tekst pierwszy",
-        "Tekst drugi",
-        "Tekst trzeci",
-        "Tekst czwarty",
-    ];
+    // const texts = [
+    //     "Tekst pierwszy",
+    //     "Tekst drugi",
+    //     "Tekst trzeci",
+    //     "Tekst czwarty",
+    // ];
 
 
     return (
-<>
-    <div className="bg-custom-pattern bg-cover">
-        <div className="max-w-7xl m-auto min-h-80 p-9 text-xl flex flex-col justify-center items-center">
-            <h1 className="text-4xl font-bold text-white text-left animate-fadeIn">Kwiaciarnia Róża</h1>
-            <div className="overflow-hidden">
-                <h2 className="text-2xl text-white font-bold animate-fadeIn">Spraw radość twoim bliskim</h2>
+        <>
+            <div className="bg-custom-pattern bg-cover">
+                <div className="max-w-7xl m-auto min-h-80 p-9 text-xl flex flex-col justify-center items-center">
+                    <h1 className="text-4xl font-bold text-white text-left animate-fadeIn">Kwiaciarnia Róża</h1>
+                    <div className="overflow-hidden">
+                        <h2 className="text-2xl text-white font-bold animate-fadeIn">Spraw radość twoim bliskim</h2>
+                        {/*<button type="button"*/}
+                        {/*        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Default*/}
+                        {/*</button>*/}
+                        {/*<Swiper*/}
+                        {/*    spaceBetween={50}*/}
+                        {/*    slidesPerView={1}*/}
+                        {/*    loop={true}*/}
+                        {/*>*/}
+                        {/*    {texts.map((text, index) => (*/}
+                        {/*        <SwiperSlide key={index}>*/}
+                        {/*            <h2>{text}</h2>*/}
+                        {/*        </SwiperSlide>*/}
+                        {/*    ))}*/}
+                        {/*</Swiper>*/}
 
-                <Swiper
-                    spaceBetween={50}
-                    slidesPerView={1}
-                    loop={true}
-                >
-                    {texts.map((text, index) => (
-                        <SwiperSlide key={index}>
-                            <h2>{text}</h2>
-                        </SwiperSlide>
-                    ))}
-                </Swiper>
-
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-    <main className="max-w-screen-xl mx-auto text-center px-9">
-        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis commodi est odio perspiciatis quis
-            ullam veniam! Accusantium aliquam aperiam, architecto dignissimos eos mollitia non possimus quos sed tenetur
-            velit voluptatem.
-        </div>
-        <div>Accusantium aliquid amet beatae dolores eum id libero mollitia odio temporibus vel. Exercitationem id iste
-            necessitatibus. A fuga nulla officia quia quibusdam similique voluptatem! Autem quae qui ratione tenetur
-            velit.
-        </div>
-        <div>Accusantium aliquam deleniti distinctio dolorem ea eos exercitationem in maiores necessitatibus nihil,
-            nostrum odio perferendis praesentium provident reiciendis sapiente sint? Atque dolorem dolorum illum
-            inventore ipsa nostrum odio omnis provident!
-        </div>
-        <div>Deleniti doloremque qui saepe tempora? Cumque distinctio dolorem ducimus eaque expedita minima quasi unde.
-            Aliquam culpa laboriosam magnam quasi quibusdam repellendus sit tempora, ullam! Corporis expedita numquam
-            praesentium vel vitae?
-        </div>
-        <div>Accusantium, atque cum cumque debitis deserunt dolor dolore dolorum eius excepturi fugit incidunt minus
-            nostrum optio pariatur quaerat quibusdam, totam voluptatem voluptatibus. A, ab aliquid dolore doloribus ea
-            ipsum iusto.
-        </div>
-    </main>
-</>
+            <main className="max-w-screen-xl mx-auto text-center px-9">
+                <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis commodi est odio perspiciatis
+                    quis
+                    ullam veniam! Accusantium aliquam aperiam, architecto dignissimos eos mollitia non possimus quos sed
+                    tenetur
+                    velit voluptatem.
+                </div>
+                <div>Accusantium aliquid amet beatae dolores eum id libero mollitia odio temporibus vel. Exercitationem
+                    id iste
+                    necessitatibus. A fuga nulla officia quia quibusdam similique voluptatem! Autem quae qui ratione
+                    tenetur
+                    velit.
+                </div>
+                <div>Accusantium aliquam deleniti distinctio dolorem ea eos exercitationem in maiores necessitatibus
+                    nihil,
+                    nostrum odio perferendis praesentium provident reiciendis sapiente sint? Atque dolorem dolorum illum
+                    inventore ipsa nostrum odio omnis provident!
+                </div>
+                <div>Deleniti doloremque qui saepe tempora? Cumque distinctio dolorem ducimus eaque expedita minima
+                    quasi unde.
+                    Aliquam culpa laboriosam magnam quasi quibusdam repellendus sit tempora, ullam! Corporis expedita
+                    numquam
+                    praesentium vel vitae?
+                </div>
+                <div>Accusantium, atque cum cumque debitis deserunt dolor dolore dolorum eius excepturi fugit incidunt
+                    minus
+                    nostrum optio pariatur quaerat quibusdam, totam voluptatem voluptatibus. A, ab aliquid dolore
+                    doloribus ea
+                    ipsum iusto.
+                </div>
+            </main>
+        </>
     )
 }
 
