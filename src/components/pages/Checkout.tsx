9c36e2a81c97141ad406e7bc983520447742bb2f
kpietryga <krzysztof.pietryga@gmail.com>
2025-01-09 22:09:53 +0100

    add inputs in to check out

--- src/components/pages/Checkout.tsx
+++ src/components/pages/Checkout.tsx
@@ -14,41 +14,50 @@ const useInput = (initialValue: string) => {
             setTouched(false);
         },
         bind: {
-        value,
+            value,
             onChange: (e) => {
-            setValue(e.target.value);
-            setTouched(true);
+                setValue(e.target.value);
+                setTouched(true);
             }
         }
     }
-    }
+}
 
-    const Checkout: React.FC = () => {
-        const {
-            value: firstName,
-            touched: touchedFirstName,
-            bind: bindFirstName,
-            reset: resetFirstName
-        } = useInput('');
+const Checkout: React.FC = () => {
+    const {
+        value: firstName,
+        touched: touchedFirstName,
+        bind: bindFirstName,
+        reset: resetFirstName
+    } = useInput('');
 
-        return (
-            <>
-                <TopTitle title="Zamówenie" subTitle="Poznaj nasze usługi"/>
-                <main className="max-w-screen-xl mx-auto text-justify p-12 animate-fadeIn">
-                    <div>
-                        <label>Imię</label>
-                        <input type='text' className="form-control" {...bindFirstName} />
-                        <br/>
-                        {/*{*/}
-                        {/*    !error.firstName &&*/}
-                        {/*    touchedFirstName &&*/}
-                        {/*    <label className="text-danger">First Name error</label>*/}
-                        {/*}*/}
+    return (
+        <>
+            <TopTitle title="Zamówenie" subTitle="Poznaj nasze usługi"/>
+            <main className="max-w-screen-xl mx-auto text-justify p-12 animate-fadeIn">
+                <div>
+                    <div className="py-4"><label>Imię</label>
+                        <input type='text' className="form-control border rounded-xl ml-2 border-gray-600" {...bindFirstName} />
+                    </div>
+                    <div className="py-4"><label>Nazwisko</label>
+                        <input type='text' className="form-control border rounded-xl ml-2 border-gray-600" {...bindFirstName} />
+                    </div>
+                    <div className="py-4"><label>Adres</label>
+                        <input type='text' className="form-control border rounded-xl ml-2 border-gray-600" {...bindFirstName} />
                     </div>
 
-                </main>
-            </>
-        )
-    };
+
+                    <br/>
+                    {/*{*/}
+                    {/*    !error.firstName &&*/}
+                    {/*    touchedFirstName &&*/}
+                    {/*    <label className="text-danger">First Name error</label>*/}
+                    {/*}*/}
+                </div>
+
+            </main>
+        </>
+    )
+};
 
 export default Checkout;
