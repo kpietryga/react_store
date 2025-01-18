4e3f4b6d50923cf0a478a5dab9950232445b536a
kpietryga <krzysztof.pietryga@gmail.com>
2025-01-18 22:10:27 +0100

    Add styles to accordion

--- src/components/common/ContactAccordion.tsx
+++ src/components/common/ContactAccordion.tsx
@@ -1,35 +1,64 @@
-import {Accordion, AccordionItem} from '@szhsin/react-accordion';
+import {Accordion, AccordionItem as Item} from "@szhsin/react-accordion";
+import {ChevronDownIcon} from "@heroicons/react/16/solid";
 
-export default function ContactAccordion() {
+/**
+ * @type {React.ExoticComponent<import('@szhsin/react-accordion').AccordionItemProps>}
+ */
+const AccordionItem = ({header, ...rest}) => (
+    <Item
+        {...rest}
+        header={({state: {isEnter}}) => (
+            <>
+                {header}
+                <ChevronDownIcon  className={`w-8 h-8 ml-auto transition-transform duration-200 ease-out ${
+                    isEnter && "rotate-180"
+                }`}/>
+
+            </>
+        )}
+        className="border-b "
+        buttonProps={{
+            className: ({isEnter}) =>
+                `flex font-bold  items-center w-full p-4 text-left hover:bg-customRed ${
+                    isEnter && "bg-customRed"
+                }`
+        }}
+        contentProps={{
+            className: "transition-height duration-200 ease-out"
+        }}
+        panelProps={{className: "p-4"}}
+    />
+);
+
+export default function App() {
     return (
-        <>
-            <h2>Najczęściej zadawane Pytania</h2>
-
-            <Accordion className="mt-5 border">
-                <AccordionItem className="border" header={
-                    <div>
-                        <p className="border">What is Lorem Ipsum?</p>
-                        <p className="bg-customRed">
-                            Lorem ipsum is a placeholder text commonly used to
-                            demonstrate the visual form of a document.
-                        </p>
-                    </div>
-                }>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        <div className="mx-2 mb-9 border-t">
+            {/* `transitionTimeout` prop should be equal to the transition duration in CSS */}
+            <Accordion transition transitionTimeout={200}>
+                <AccordionItem header="What is Lorem Ipsum?" initialEntered>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 </AccordionItem>
 
-                <AccordionItem className="border" header="Where does it come from?">
-                    Quisque eget luctus mi, vehicula mollis lorem. Proin fringilla
-                    vel erat quis sodales. Nam ex enim, eleifend venenatis lectus
-                    vitae, accumsan auctor mi.
+                <AccordionItem header="Where does it come from?">
+                    Quisque eget luctus mi, vehicula mollis lorem. Proin fringilla vel
+                    erat quis sodales. Nam ex enim, eleifend venenatis lectus vitae.Quisque eget luctus mi, vehicula mollis lorem. Proin fringilla vel
+                    erat quis sodales. Nam ex enim, eleifend venenatis lectus vitae.Quisque eget luctus mi, vehicula mollis lorem. Proin fringilla vel
+                    erat quis sodales. Nam ex enim, eleifend venenatis lectus vitae.Quisque eget luctus mi, vehicula mollis lorem. Proin fringilla vel
+                    erat quis sodales. Nam ex enim, eleifend venenatis lectus vitae.
                 </AccordionItem>
 
-                <AccordionItem className="border" header="Why do we use it?">
-                    Suspendisse massa risus, pretium id interdum in, dictum sit
-                    amet ante. Fusce vulputate purus sed tempus feugiat.
+                <AccordionItem header="Why do we use it?">
+                    Suspendisse massa risus, pretium id interdum in, dictum sit amet ante.
+                    Fusce vulputate purus sed tempus feugiat.Suspendisse massa risus, pretium id interdum in, dictum sit amet ante.
+                    Fusce vulputate purus sed tempus feugiat.Suspendisse massa risus, pretium id interdum in, dictum sit amet ante.
+                    Fusce vulputate purus sed tempus feugiat.Suspendisse massa risus, pretium id interdum in, dictum sit amet ante.
+                    Fusce vulputate purus sed tempus feugiat.
                 </AccordionItem>
             </Accordion>
-        </>
+        </div>
     );
 }
