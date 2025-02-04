6156817d6d048a6286f805d57b0cb95946406f29
kpietryga <krzysztof.pietryga@gmail.com>
2025-02-04 23:50:14 +0100

    Delete lorem ipsum

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -20,29 +20,6 @@ const Home: React.FC = () => {
                 <h2 className="text-2xl font-bold text-customRed m-10">Polecane</h2>
                     <PromotedProducts/>
                     <div className="my-9 flex gap-5 text-justify">
-                        <div>Accusantium aliquid amet beatae dolores eum id libero mollitia odio temporibus vel.
-                            Exercitationem
-                            id iste
-                            necessitatibus. A fuga nulla officia quia quibusdam similique voluptatem! Autem quae qui
-                            ratione
-                            tenetur
-                            velit.
-                        </div>
-                        <div>Accusantium aliquam deleniti distinctio dolorem ea eos exercitationem in maiores
-                            necessitatibus
-                            nihil,
-                            nostrum odio perferendis praesentium provident reiciendis sapiente sint? Atque dolorem
-                            dolorum illum
-                            inventore ipsa nostrum odio omnis provident!
-                        </div>
-                        <div>Deleniti doloremque qui saepe tempora? Cumque distinctio dolorem ducimus eaque expedita
-                            minima
-                            quasi unde.
-                            Aliquam culpa laboriosam magnam quasi quibusdam repellendus sit tempora, ullam! Corporis
-                            expedita
-                            numquam
-                            praesentium vel vitae?
-                        </div>
                     </div>
             </main>
         </>
